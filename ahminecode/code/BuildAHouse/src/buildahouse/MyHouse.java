0be1edcc7afa607be5da290ca51ac11c6c3afa69
'gusdelact' <gusdelact@gmail.com>
2018-02-26 20:40:00 -0600

    Ejemplos de plugins de Minecraft para correr con Spigot

---
+++ ahminecode/code/BuildAHouse/src/buildahouse/MyHouse.java
@@ -0,0 +1,22 @@
+package buildahouse;
+/***
+ * Excerpted from "Learn to Program with Minecraft Plugins",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/ahmine for more book information.
+***/
+
+public class MyHouse {
+  public static void build_me() {
+    // Declare width
+    // Set width to the number of blocks
+    // Declare height
+    // Set height to the number of blocks
+	
+    int width=50;
+	int height=10;
+	BuildAHouse.buildMyHouse(width, height);
+  }
+}
